27f8f8f5ef8cec7228111595a2aa82ada1b13bdd
Yamaguchi Hiromitsu <yamagame@mac.com>
2021-01-10 18:00:12 +0900

    code format

--- react-router-sample/src/index.tsx
+++ react-router-sample/src/index.tsx
@@ -1,36 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { createStore as reduxCreateStore, Reducer } from "redux";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import {
+  createStore as reduxCreateStore,
+  Reducer,
+} from 'redux'
 
 interface AppState {
-  state: any;
+  state: any
 }
 
 export type Action = {
-  type: "change state";
-  payload: AppState;
-};
+  type: 'change state'
+  payload: AppState
+}
 
-export const appState: Reducer<AppState, Action> = (state, action) => {
+export const appState: Reducer<AppState, Action> = (
+  state,
+  action
+) => {
   if (state) {
     switch (action.type) {
-      case "change state":
+      case 'change state':
         return {
           ...state,
           ...action.payload,
-        } as AppState;
+        } as AppState
       default:
-        return state;
+        return state
     }
   }
-  return { state: "" };
-};
+  return { state: '' }
+}
 
-const store = reduxCreateStore(appState, { state: "" });
+const store = reduxCreateStore(appState, { state: '' })
 
 ReactDOM.render(
   <React.StrictMode>
@@ -38,10 +44,10 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
