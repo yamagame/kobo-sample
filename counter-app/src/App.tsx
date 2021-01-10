f3011b1d8c85ce8ec6f5a9a5fe68b232a690b99b
Yamaguchi Hiromitsu <yamagame@mac.com>
2021-01-10 18:16:38 +0900

    add global state counter

--- counter-app/src/App.tsx
+++ counter-app/src/App.tsx
@@ -1,23 +1,55 @@
 import React from 'react'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+
+type CounterProps = {
+  state: number
+  onClick: () => void
+}
+
+const Counter: React.FC<CounterProps> = ({
+  state,
+  onClick,
+}) => {
+  return (
+    <>
+      <p>{state}</p>
+      <button onClick={onClick}>カウントアップ</button>
+    </>
+  )
+}
 
 function App() {
+  const dispatch = useDispatch()
+  const selector: any = useSelector((state) => state)
   const [
     componentCounterState,
     setComponentCounterState,
   ] = React.useState(0)
   return (
     <div>
-      <p>{componentCounterState}</p>
-      <button
+      <Counter
+        state={componentCounterState}
         onClick={() =>
           setComponentCounterState(
             componentCounterState + 1
           )
         }
-      >
-        カウントアップ
-      </button>
+      />
+      <Counter
+        state={selector.state.globalCounterState}
+        onClick={() =>
+          dispatch({
+            type: 'change state',
+            payload: {
+              state: {
+                globalCounterState:
+                  selector.state.globalCounterState + 1,
+              },
+            },
+          })
+        }
+      />
     </div>
   )
 }
--- counter-app/src/index.tsx
+++ counter-app/src/index.tsx
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import {
+  createStore as reduxCreateStore,
+  Reducer,
+} from 'redux'
+
+interface AppState {
+  state: {
+    globalCounterState: number
+  }
+}
+
+const initialState: AppState = {
+  state: {
+    globalCounterState: 0,
+  },
+}
+
+export type Action = {
+  type: 'change state'
+  payload: AppState
+}
+
+export const appState: Reducer<AppState, Action> = (
+  state,
+  action
+) => {
+  if (state) {
+    switch (action.type) {
+      case 'change state':
+        return {
+          ...state,
+          ...action.payload,
+        } as AppState
+      default:
+        return state
+    }
+  }
+  return initialState
+}
+
+const store = reduxCreateStore(appState, initialState)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
